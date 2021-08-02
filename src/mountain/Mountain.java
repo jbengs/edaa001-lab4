a59602be69b4a9054caacc1ac5d87437b4c57647
Joel Bengs <joel.bengs@gmail.com>
2021-08-02 12:09:35 +0200

    D4 done (randomness)

--- src/mountain/Mountain.java
+++ src/mountain/Mountain.java
@@ -4,9 +4,11 @@ import fractal.Fractal;
 import fractal.TurtleGraphics;
 
 public class Mountain extends Fractal {
+    private double dev;
 
-    public Mountain() {
+    public Mountain(Double dev) {
         super();
+        this.dev = dev;
     }
     /**
      * Returns the title.
@@ -29,25 +31,25 @@ public class Mountain extends Fractal {
         Point p2 = new Point(300, 200);
         Point p3 = new Point(500, 500);
         turtle.moveTo(p1.getX(), p1.getY());
-        fractalTriangle(turtle, this.order, p1, p2, p3);
+        fractalTriangle(turtle, this.order, p1, p2, p3, dev);
     }
 
-    private void fractalTriangle(TurtleGraphics turtle, int order, Point pA, Point pB, Point pC) {
+    private void fractalTriangle(TurtleGraphics turtle, int order, Point pA, Point pB, Point pC, double dev) {
         if (order == 0) {
-            //rita en triangel mellan punkterna
+            //ritar en triangel mellan punkterna
             turtle.moveTo(pA.getX(), pA.getY());
             turtle.forwardTo(pB.getX(), pB.getY());
             turtle.forwardTo(pC.getX(), pC.getY());
             turtle.forwardTo(pA.getX(), pA.getY());
         } else {
-            Point pAB = new Point((pA.getX() + pB.getX())/2, (pA.getY() + pB.getY())/2 );
-            Point pBC = new Point((pB.getX() + pC.getX())/2, (pB.getY() + pC.getY())/2);
-            Point pCA = new Point((pA.getX() + pC.getX())/2, (pA.getY() + pC.getY())/2 );
+            Point pAB = new Point((int) RandomUtilities.randFunc(dev) + (pA.getX() + pB.getX())/2, (int) RandomUtilities.randFunc(dev) + (pA.getY() + pB.getY())/2 );
+            Point pBC = new Point((int) RandomUtilities.randFunc(dev) + (pB.getX() + pC.getX())/2, (int) RandomUtilities.randFunc(dev) + (pB.getY() + pC.getY())/2);
+            Point pCA = new Point((int) RandomUtilities.randFunc(dev) + (pA.getX() + pC.getX())/2, (int) RandomUtilities.randFunc(dev) + (pA.getY() + pC.getY())/2 );
 
-            fractalTriangle(turtle, order-1, pA, pAB, pCA);
-            fractalTriangle(turtle, order-1, pAB, pB, pBC);
-            fractalTriangle(turtle, order-1, pCA, pBC, pC);
-            fractalTriangle(turtle,order-1, pAB, pBC, pCA);
+            fractalTriangle(turtle, order-1, pA, pAB, pCA, dev/2);
+            fractalTriangle(turtle, order-1, pAB, pB, pBC, dev/2);
+            fractalTriangle(turtle, order-1, pCA, pBC, pC, dev/2);
+            fractalTriangle(turtle,order-1, pAB, pBC, pCA, dev/2);
         }
     }
 }
